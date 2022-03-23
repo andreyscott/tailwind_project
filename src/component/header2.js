fc6a637617d74ed3f36adaade499f85202090a0a
andreyscott <andreyscott>
2022-03-23 10:26:02 +0100

    made it worked now

--- src/component/header2.js
+++ src/component/header2.js
@@ -20,23 +20,22 @@ export default function Header2() {
             <div className="flex items-center justify-between">
                 <div>
                     <a className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Brand</a>
+                 
                 </div>
                 
                 <div className="flex md:hidden">
-                    <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                        <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                            <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                        </svg>
+                    <button type="button" onClick={ () => setIsOpen(!isOpen) }  className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <ion-icon className="text-5xl" name={isOpen ? 'close':'menu'}></ion-icon>
                     </button>
                 </div>
             </div>
 
-            <div className="items-center md:flex">
-                <div className="flex flex-col md:flex-row md:mx-6">
+            <div className={`items-center md:flex ${ isOpen ? 'block' : 'hidden'}`}>
+                <div className="flex flex-col md:flex-row md:mx-6 ">
                 {
                     Links.map((link, index) => {
                         return (
-                            <a className="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href={link.link} key={index}>{link.name}</a>
+                            <a className="my-1 p-2 text-md font-medium text-gray-700 transition-all duration-5000 ease-in-out transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href={link.link} key={index}>{link.name}</a>
                         )
                     })
                 }
